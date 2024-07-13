c9f2deb06af54a4b3cf4817224fb0fb3746d1d74
jayzsh <55305804+jayzsh@users.noreply.github.com>
2024-07-13 16:47:29 +0530

    Fix bug: duplicate of Postflight renderig as Preflight CiMessage
    Co-authored-by: Stuti Pradhan <StutiPradhan@users.noreply.github.com>

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -36,7 +36,8 @@ function ChatSection() {
         { chatRoomName: chatRoomName!, password: localChatRoom!.password, user_token_hash: userTokenHash },
         {
           onData(data) {
-            setNewMsgList((prev) => [...prev, new CiMessagePostflight({ messageBody: data })]);
+            const peerMessage = new CiMessagePostflight({ messageBody: data });
+            setNewMsgList((prev) => [...prev, peerMessage]);
           },
           onError(err) {
             console.error('Subscription error:', err);
@@ -51,7 +52,7 @@ function ChatSection() {
   }, [chatRoomName, userProfile]);
 
   useEffect(() => {
-    document.getElementById('msg-container')?.scrollTo({ top: 99999999 });
+    document.getElementById('msg-container')?.scrollTo({ top: Number.MAX_SAFE_INTEGER });
   }, [newMsgList, data]);
 
   useEffect(() => {
@@ -88,6 +89,52 @@ function ChatSection() {
       );
   }
 
+  function CiMessageBody({ ciMessage, index }: { ciMessage: CiMessagePostflight | CiMessagePreflight; index: number }) {
+    if (ciMessage instanceof CiMessagePostflight) {
+      return (
+        <li key={ciMessage.key}>
+          {getMessageGroupSeparatorByDay({
+            previousCreatedAt:
+              index === 0
+                ? new Date(data!.payload!.messages.slice(-1)[0]?.createdAt)
+                : new Date(newMsgList[index - 1].createdAt),
+            createdAt: new Date(ciMessage.createdAt),
+          })}
+          <span className="text-gray-600 text-xs">{getFormattedHourMin(new Date(ciMessage.createdAt))}</span>
+          &nbsp;
+          <span className="text-purple-400">{ciMessage.messageBody.sender_username} </span>
+          {ciMessage.messageBody.content}
+        </li>
+      );
+    } else if (ciMessage instanceof CiMessagePreflight) {
+      return (
+        <li key={ciMessage.key}>
+          {getMessageGroupSeparatorByDay({
+            previousCreatedAt:
+              index === 0
+                ? new Date(data!.payload!.messages.slice(-1)[0]?.createdAt)
+                : new Date(newMsgList[index - 1].createdAt),
+            createdAt: new Date(ciMessage.createdAt),
+          })}
+          <span className="text-gray-600 text-xs">{getFormattedHourMin(new Date(ciMessage.createdAt))}</span>
+          &nbsp;
+          <span className="text-amber-300">{ciMessage.messageBody.sender_username}</span>
+          <>
+            {ciMessage.isSent === false && ciMessage.isFailed === false && (
+              <span className="text-gray-400">{ciMessage.messageBody.content}</span>
+            )}
+            {ciMessage.isSent === false && ciMessage.isFailed === true && (
+              <button onClick={ciMessage.retry} className="text-red-500">
+                {ciMessage.messageBody.content}
+              </button>
+            )}
+            {ciMessage.isSent === true && ciMessage.isFailed === false && <span>{ciMessage.messageBody.content}</span>}
+          </>
+        </li>
+      );
+    }
+  }
+
   return (
     <div className="bg-gray-700 flex flex-col">
       <h2 className="sticky text-center">Chats</h2>
@@ -114,48 +161,9 @@ function ChatSection() {
               </li>
             ))}
 
-            {newMsgList.map((message, index) =>
-              message instanceof CiMessagePostflight ? (
-                <li key={message.key}>
-                  {getMessageGroupSeparatorByDay({
-                    previousCreatedAt:
-                      index === 0
-                        ? new Date(data.payload!.messages.slice(-1)[0]?.createdAt)
-                        : new Date(newMsgList[index - 1].createdAt),
-                    createdAt: new Date(message.createdAt),
-                  })}
-                  <span className="text-gray-600 text-xs">{getFormattedHourMin(new Date(message.createdAt))}</span>
-                  <span className="text-gray-500">{message.messageBody.sender_username} </span>
-                  {message.messageBody.content}
-                </li>
-              ) : (
-                <li key={message.key}>
-                  {getMessageGroupSeparatorByDay({
-                    previousCreatedAt:
-                      index === 0
-                        ? new Date(data.payload!.messages.slice(-1)[0]?.createdAt)
-                        : new Date(newMsgList[index - 1].createdAt),
-                    createdAt: new Date(message.createdAt),
-                  })}
-                  <span className="text-gray-600 text-xs">{getFormattedHourMin(new Date(message.createdAt))}</span>
-                  &nbsp;
-                  <span className="text-amber-300">{message.messageBody.sender_username} </span>
-                  <>
-                    {message.isSent === false && message.isFailed === false && (
-                      <span className="text-gray-400">{message.messageBody.content}</span>
-                    )}
-                    {message.isSent === false && message.isFailed === true && (
-                      <button onClick={message.retry} className="text-red-500">
-                        {message.messageBody.content}
-                      </button>
-                    )}
-                    {message.isSent === true && message.isFailed === false && (
-                      <span>{message.messageBody.content}</span>
-                    )}
-                  </>
-                </li>
-              ),
-            )}
+            {newMsgList.map((message, index) => (
+              <CiMessageBody ciMessage={message} key={message.key} index={index} />
+            ))}
           </ul>
         )}
       </div>
